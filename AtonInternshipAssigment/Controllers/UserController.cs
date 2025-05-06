58a89ab4c62b659605c19c605a879fad06555908
Иван Завадский <zavadskiy1998@yandex.ru>
2025-05-06 22:46:39 +0400

    new

--- AtonInternshipAssigment/Controllers/UserController.cs
+++ AtonInternshipAssigment/Controllers/UserController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AtonInternshipAssigment.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AtonInternshipAssigment.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/[actoin]")]
+    [Route("api/[controller]/[action]")]
     public class UserController : Controller
     {
-        
+        private readonly UserRepository _userRepository;
+
+        public UserController(UserRepository userRepository) 
+        { 
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(string login, string password, string newUserLogin, string newUserPassword, string newUserName, int newUserGender, DateTime Birthday, bool admin)
+        {
+
+        }
     }
 }
--- AtonInternshipAssigment/Repositories/UserRepository.cs
+++ AtonInternshipAssigment/Repositories/UserRepository.cs
@@ -1,6 +1,57 @@
-﻿namespace AtonInternshipAssigment.Repositories
+﻿using AtonInternshipAssigment.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace AtonInternshipAssigment.Repositories
 {
     public class UserRepository
     {
+        private readonly string _connectionString;
+
+        public UserRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        // Создание пользователя (для администраторов)
+        public async Task CreateUser(User user, string createdBy)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            user.CreatedBy = createdBy;
+            user.ModifiedBy = createdBy;
+            user.ModifiedOn = DateTime.UtcNow;
+
+            await connection.ExecuteAsync(
+                @"INSERT INTO Users 
+            (Guid, Login, Password, Name, Gender, Birthday, Admin, 
+             CreatedOn, CreatedBy, ModifiedOn, ModifiedBy) 
+            VALUES 
+            (@Guid, @Login, @Password, @Name, @Gender, @Birthday, @Admin, 
+             @CreatedOn, @CreatedBy, @ModifiedOn, @ModifiedBy)",
+                user);
+        }
+
+        // Получение активных пользователей (для администраторов)
+        public async Task<IEnumerable<User>> GetActiveUsers()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            return await connection.QueryAsync<User>(
+                "SELECT * FROM Users WHERE RevokedOn IS NULL ORDER BY CreatedOn");
+        }
+
+        public async Task UserIsAdmin(string login, string password)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            try
+            {
+                connection.QueryAsync(
+                    "SELECT Login, Password FROM Users WHERE");
+            }
+            catch (Exception ex) 
+            { 
+            }
+        }
+
     }
 }
